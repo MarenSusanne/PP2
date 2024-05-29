a9f678e7acf4e63037c0fbb89f3f4ac19a223468
MarenSusanne <Marensut@getacademy.no>
2024-05-29 13:47:17 +0200

    Add project files.

---
+++ PP2/Movies.cs
@@ -0,0 +1,31 @@
+﻿namespace PP2;
+
+public class Movies
+
+    {
+    public string Title;
+    public int Year;
+    public string Description;
+    public string Director;
+    public string Actors;
+
+    public Movies(string title, int year, string description, string director, string actors)
+    {
+        Title = title;
+        Year = year;
+        Description = description;
+        Director = director;
+        Actors = actors;
+    }
+
+    public void MovieInfo()
+    {
+        Console.WriteLine($"Tittel: {Title}");
+        Console.WriteLine("År: " + Year);
+        Console.WriteLine($"Beskrivelse: {Description}");
+        Console.WriteLine($"Regissør: {Director}");
+        Console.WriteLine($"Skuespillere: {Actors}");
+    }
+
+}
+
---
+++ PP2/Program.cs
@@ -0,0 +1,47 @@
+﻿//    Lag en applikasjon der man kan skrive inn forskjellige detaljer, og etter man har skrevet inn alt få 
+//    printet tilbake det som ble skrevet inn som et “produkt”, enten en film eller en bok, dere velger.
+//    Man skal bli spurt av konsollen om Tittel, så år det kom ut, litt beskrivelse av hva filmen eller boken handler om, 
+//    forfatter/regissør, hvilke skuespillere som var med.
+//    Når alle spørsmål er besvart skal man kunne få se boken eller filmen man skrev inn som et produkt, med alle egenskaper listet opp.
+
+
+using PP2;
+CoolMovies();
+static void CoolMovies()
+{
+    Console.WriteLine("Få informasjon om en film, ved å skrive inn det vi spør om!");
+
+    Console.WriteLine("Tittel:");
+
+    var input1 = Console.ReadLine();
+
+    Console.WriteLine("År:");
+    var input2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Beskrivelse:");
+    var input3 = Console.ReadLine();
+
+    Console.WriteLine("Regissør:");
+    var input4 = Console.ReadLine();
+
+    Console.WriteLine("Skuespillere:");
+    var input5 = Console.ReadLine();
+
+
+    var myMovie = new Movies(input1, input2, input3, input4, input5);
+
+
+    myMovie.MovieInfo();
+
+    Console.WriteLine("Vil du legge til en ekstra film, si ja");
+    var inputAnswer = Console.ReadLine();
+
+    if (inputAnswer == "ja")
+    {
+        CoolMovies();
+    }
+    else
+    {
+        Console.Clear();
+    }
+}
